404893008f31050ee83f1fabb77fb71b1388543d
Eric Laurello <eric.laurello@flipsidecrypto.com>
2024-10-09 17:01:54 -0400

    prod api integration

--- macros/streamline/streamline_udfs.sql
+++ macros/streamline/streamline_udfs.sql
@@ -3,7 +3,7 @@
     OR REPLACE EXTERNAL FUNCTION streamline.udf_bulk_rest_api_v2(
         json OBJECT
     ) returns ARRAY api_integration = {% if target.name == "prod" %}
-        aws_aleo_api_prod_v2 AS 'https://gvmfebiq6g.execute-api.us-east-1.amazonaws.com/stg/udf_bulk_rest_api' -- CHANGE TO PROD URL WHEN DEPLOYED
+        aws_aleo_api_prod_v2 AS 'https://onyzmq60n9.execute-api.us-east-1.amazonaws.com/prod/udf_bulk_rest_api'
     {% else %}
         aws_aleo_api_stg_v2 AS 'https://gvmfebiq6g.execute-api.us-east-1.amazonaws.com/stg/udf_bulk_rest_api'
     {%- endif %};
